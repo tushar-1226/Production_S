2e255ef8e332361c22b14d65d0983b6c343644c5
shivamk-tech <skkumarsv786@gmail.com>
2026-01-26 12:54:49 +0530

    some changes

--- Frontend/Uber/src/components/Plan.jsx
+++ Frontend/Uber/src/components/Plan.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import watch from '../assets/watch.png'
-import { CalendarIcon } from "@heroicons/react/24/solid";
+import CalendarIcon from '../assets/calendar.svg'
+import Clock from '../assets/clock.svg';
+import CreditCard from '../assets/CreditCard.svg';
 
 
 const Plan = () => {
-        console.log("hello");
+    console.log("hello");
 
 
     return (
@@ -13,7 +15,7 @@ const Plan = () => {
                 Plan for later
             </div>
             <div >
-                <div className=''>
+                <div className='flex gap-7'>
                     <div className='bg-[#9AD4DE] rounded-xl h-100 relative w-192 pt-17 pl-10'>
                         <div className='flex flex-col gap-10 w-93'>
                             <div className='text-4xl font-bold w-80' style={{ fontFamily: "Inter, system-ui, sans-serif" }}>Get your ride right with Uber Reserve</div>
@@ -26,14 +28,14 @@ const Plan = () => {
                                         <div>
                                             <span className='text-sm text-gray-500'>Date</span>
                                             <div className='h-12 w-45 bg-white rounded-[10px] flex items-center p-4 gap-5 cursor-pointer'>
-                                                <i className="fa-solid fa-calendar"></i>
+                                                <div className='h-4 w-4 overflow-hidden'><img src={CalendarIcon} alt="" className='object-cover' /></div>
                                                 <input className='h-7 w-20 outline-none cursor-pointer' type="text" name="" id="" />
                                             </div>
                                         </div>
                                         <div>
                                             <span className='text-sm text-gray-500'>Time</span>
                                             <div className='h-12 w-45 bg-white rounded-[10px] flex items-center p-4 gap-5 cursor-pointer'>
-                                                <i class="fa-solid fa-clock"></i>
+                                                <i className="fa-solid fa-clock"></i>
                                                 <input className='h-7 w-20 outline-none cursor-pointer' type="text" name="" id="" />
                                             </div>
                                         </div>
@@ -44,16 +46,46 @@ const Plan = () => {
                                 </div>
                             </div>
                         </div>
-                        <img className='absolute bottom-0 right-0' src={watch} height={100} width={270} />
+                        <div className='h-20 w-50'>
+                            <img className='absolute bottom-0 right-0 object-cover' src={watch} />
+                        </div>
                     </div>
                     {/* Benefits section */}
-                    <div className=''>
-                        <div className='text-2xl font-bold' style={{ fontFamily: "Inter, system-ui, sans-serif" }}>
+                    <div className='border-1 border-gray-100 h-85 w-90 pt-4 px-4 rounded-[10px] flex flex-col gap-6'>
+                        <div className='text-xl font-bold' style={{ fontFamily: "Inter, system-ui, sans-serif" }}>
                             Benefits
                         </div>
-                        <div></div>
-                        <div></div>
-
+                        <div className='pl-6 flex flex-col gap-4 items-center'>
+                            <div className='flex items-center gap-5'>
+                                <div className='w-8 h-5 '>
+                                    <img src={CalendarIcon} alt="" className="object-cover" />
+                                </div>
+                                <div className='text-base font-normal'>
+                                    Choose your exact pickup time up to 90 days in advance.
+                                </div>
+                            </div>
+                            <div className='h-[1px] w-56.5 bg-gray-100'></div>
+                            <div className='flex items-center gap-5'>
+                                <div className='w-8 h-5'>
+                                    <img src={Clock} alt="" className="object-cover" />
+                                </div>
+                                <div className='text-base font-normal'>
+                                    Extra wait time included to meet your ride.
+                                </div>
+                            </div>
+                            <div className='h-[1px] w-56.5 bg-gray-100'></div>
+                            <div className='flex items-center gap-5'>
+                               <div className='w-8 h-5'>
+                                    <img src={CreditCard} alt="" className="object-cover" />
+                                </div>
+                                <div className='text-base font-normal'>
+                                    Cancel at no charge up to 60 minutes in advance.
+                                </div>
+                            </div>
+                        </div>
+                        <div className='font-light underline decoration-dotted underline-offset-5'>
+                            See terms
+                        </div>
                     </div>
                 </div>
                 <div>
