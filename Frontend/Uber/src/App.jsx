fa3edbb23cf82ae42681999202f4b0f0938d77f2
shivamk-tech <skkumarsv786@gmail.com>
2026-01-31 18:36:57 +0530

    updated

--- Frontend/Uber/src/App.jsx
+++ Frontend/Uber/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar.jsx'
 import SubNav from './components/SubNav.jsx'
 import Container from './components/Container.jsx'
 import SeePrices from './components/SeePrices.jsx'
+import Buisness from './components/Buisness.jsx'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <SubNav/>
       <Container/>
       <SeePrices/>
+      <Buisness/>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, eligendi architecto iste molestias saepe molestiae esse expedita natus libero consectetur eos totam excepturi reprehenderit minima praesentium quibusdam cumque voluptatibus! Perspiciatis repudiandae tenetur earum laudantium? Minus, deserunt! Quam doloribus recusandae quaerat accusantium rerum amet, aliquam inventore, blanditiis molestiae nostrum quos expedita beatae mollitia exercitationem rem architecto corporis ratione? Ratione iure sequi id, aliquam omnis odio, a perferendis ullam vitae odit aut deleniti nulla? Optio quod possimus cum perferendis porro ex sint, nesciunt commodi aut, a quia aperiam, vel vero? Qui alias unde magnam accusamus maxime architecto fuga soluta aperiam perspiciatis? Illum harum repudiandae suscipit. Optio minima deserunt magnam veniam debitis similique ullam distinctio veritatis impedit dolor numquam dicta eum iure velit ducimus totam quis quidem non delectus, ad nobis repudiandae possimus accusantium officiis? Beatae quibusdam ducimus accusamus nisi consequuntur impedit aspernatur, aut perspiciatis, praesentium amet aliquid tenetur et illo, iusto iste sit sunt eaque dolores nemo quos fuga voluptatem sequi? Error veritatis vel ullam unde quidem quod, numquam inventore eius dolores nihil necessitatibus aliquid temporibus eaque repellat dolore suscipit amet incidunt nemo consequatur in commodi iste atque. Iure voluptas omnis maiores hic beatae sit ab eos, expedita cupiditate nobis necessitatibus pariatur.
       
     </div>
---
+++ Frontend/Uber/src/components/Buisness.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Buisness = () => {
+  console.log("Buisness");
+  
+
+  return (
+    <div className='px-40 h-140 w-full bg-black my-17'>
+        <div>
+            <div className='text-white font-bold text-4xl w-100'>
+                Looking for business solutions?
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Buisness
+
