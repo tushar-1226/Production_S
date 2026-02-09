394f092d3bc453d8495cea98b6b7a0245a159929
shivamk-tech <skkumarsv786@gmail.com>
2026-02-09 07:47:37 +0530

    updated

--- Frontend/Uber/src/components/Business/BusinessLeverage.jsx
+++ Frontend/Uber/src/components/Business/BusinessLeverage.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import walkingmam from '../../assets/walkingmam.svg'
+import songsir from '../../assets/songsir.svg'
+import meal from '../../assets/meal.svg'
 
 const BusinessLeverage = () => {
     return (
-        <div className='px-40'>
+        <div className='px-40 flex flex-col gap-10 w-full py-15'>
             <div className='text-4xl font-bold w-185'>
                 How companies leverage Uber for Business
             </div>
-            <div>
+            <div className='flex w-full justify-between'>
                 <div className='flex flex-col gap-3 '>
                     <div className=''>
                         <img className='' src={walkingmam} alt="" />
@@ -18,18 +20,18 @@ const BusinessLeverage = () => {
                 </div>
                 <div>
                     <div className=''>
-                        <img className='' src={walkingmam} alt="" />
+                        <img className='' src={songsir} alt="" />
                     </div>
                     <div className='text-2xl font-bold'>
-                        Business travel
+                        Courtesy rides
                     </div>
                 </div>
                 <div>
                     <div className=''>
-                        <img className='' src={walkingmam} alt="" />
+                        <img className='' src={meal} alt="" />
                     </div>
                     <div className='text-2xl font-bold'>
-                        Business travel
+                        Meals programs
                     </div>
                 </div>
             </div>
