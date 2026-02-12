c6d9aa89aa43da3568543fda304931d0964f57ce
shivamk-tech <skkumarsv786@gmail.com>
2026-02-12 07:51:04 +0530

    updated

--- Frontend/Uber/src/components/ReqRide/ReqRidePromoDetails.jsx
+++ Frontend/Uber/src/components/ReqRide/ReqRidePromoDetails.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 const ReqRidePromoDetails = () => {
     console.log("ReqRidePromoDetails");
+    console.log("ReqRidePromoDetails");
+    console.log("ReqRidePromoDetails");
+
   return (
     <div className=' px-5 py-16 flex flex-col gap-10 lg:justify-between bg-[#F6F6F6] mt-10 '>
       <div className='flex flex-col gap-5 px-40'>
