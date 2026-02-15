916dd93699aeaa4bbba6b71f4985ca344b6d3c65
shivamk-tech <skkumarsv786@gmail.com>
2026-02-15 14:13:03 +0530

    updated

--- Frontend/Uber/src/components/AboutUs/AboutCards.jsx
+++ Frontend/Uber/src/components/AboutUs/AboutCards.jsx
@@ -8,6 +8,9 @@ const AboutCards = () => {
     console.log("about")
     console.log("about")
     console.log("about")
+    console.log("about")
+    console.log("about")
+    console.log("about")
   return (
     <div>
       
