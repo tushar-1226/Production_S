f216cd4731b5e94cac518f15c6388dcc9a6f3bc0
shivamk-tech <skkumarsv786@gmail.com>
2026-03-25 21:08:02 +0530

    updated

--- frontend/src/components/Offerings/OffHero.jsx
+++ frontend/src/components/Offerings/OffHero.jsx
@@ -15,6 +15,7 @@ const OffHero = () => {
         console.log("hellow")
         console.log("hellow")
         console.log("hellow")
+        console.log("hellow")
 
     
     const ArrayObject = [
